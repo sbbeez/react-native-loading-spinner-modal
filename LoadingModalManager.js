014f8be707f2a26e25b211a8c83fc1284a5e634e
senthilbalajiganesan <g.senthilbalaji@gmail.com>
2017-12-29 14:43:54 +0530

    Basic Usage

--- LoadingModalManager.js
+++ LoadingModalManager.js
@@ -1,30 +1,3 @@
-// "use strict";
-//
-// module.exports = {
-//   currentLoadingModal: null,
-//   registerModal(modal) {
-//     this.currentLoadingModal = modal;
-//   },
-//   unregisterModal() {
-//     this.currentLoadingModal = null;
-//   },
-//   showLoadingModal(newState = null) {
-//     if (this.currentLoadingModal === null) {
-//       return;
-//     }
-//
-//     this.hideLoadingModal();
-//
-//     this.currentLoadingModal.setNewState(newState);
-//     this.currentLoadingModal.showModal();
-//   },
-//   hideLoadingModal() {
-//     if (this.currentLoadingModal !== null) {
-//       this.currentLoadingModal.hideModal();
-//     }
-//   }
-// };
-
 class LoadingModalManager {
   constructor() {
     this.currentLoadingModal = null;
